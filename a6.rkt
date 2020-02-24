3839ae4063f10c1351fc6cc6608b8f3c390470ce
zacmonroe <zmonroe0820@gmail.com>
2020-02-24 13:17:35 -0500

    Began a6

---
+++ a6.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+; Initial continuation
+(define empty-k
+  (lambda ()
+    (let ((once-only #f))
+      (lambda (v)
+        (if once-only
+	    (error 'empty-k "You can only invoke the empty continuation once")
+	    (begin (set! once-only #t) v))))))
+
+
+; Problem 1
+(define binary-to-decimal
+  (lambda (n)
+    (cond
+      [(null? n) 0]
+      [else (+ (car n) (* 2 (binary-to-decimal (cdr n))))])))
+
