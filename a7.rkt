e1fbd29d4799a0ea4da7995b3dc0734c4949564b
zacmonroe <zmonroe0820@gmail.com>
2020-03-01 21:18:19 -0500

    Continued a7

--- a7.rkt
+++ a7.rkt
@@ -44,3 +44,42 @@
  (lambda (p) (check-equal? (last-non-zero (car p))
                            (cdr p)))
  my-lnz-data)
+
+
+
+(define (lex exp env)
+  (match exp
+    [`,y #:when (symbol? y) (list 'var (search-env env y))]
+    [`(lambda (,x) ,body)
+     (list 'lambda (lex body (cons x env)))]
+    [`(,rator ,rand)
+     `(app ,(lex rator env) ,(lex rand env))
+     #;(list (lex rator env)
+           (lex rand env))]))
+
+(define (search-env env var)
+  (cond
+    [(null? env)
+     (error (string-append "what is " (symbol->string var) "?"))]
+    [(eqv? (car env) var) 0]
+    [else (add1 (search-env (cdr env) var))]))
+
+(check-equal? (search-env '(a b c d) 'a) 0)
+(check-equal? (search-env '(a b c d) 'c) 2)
+(check-equal? (search-env '(a b c d) 'b) 1)
+(check-equal? (search-env '(a b c d) 'd) 3)
+(check-equal? (search-env '(a b c a) 'a) 0)
+(check-equal? (search-env '(a a a a) 'a) 0)
+
+(check-equal? (lex '(lambda (o) (lambda (r) (lambda (s) (lambda (p) (lambda (g) o))))) '())
+              '(lambda (lambda (lambda (lambda (lambda (var 4)))))))
+(check-equal? (lex '(lambda (x) x) '()) '(lambda (var 0)))
+(check-equal? (lex '(lambda (y) (lambda (x) y)) '()) '(lambda (lambda (var 1))))
+(check-equal? (lex '(lambda (y) (lambda (x) (x y))) '()) '(lambda (lambda ((var 0) (var 1)))))
+(check-equal? (lex '(lambda (x) (lambda (x) (x x))) '()) '(lambda (lambda ((var 0) (var 0)))))
+(check-equal? (lex '(lambda (y) ((lambda (x) (x y)) (lambda (c) (lambda (d) (y c))))) '()) 
+              '(lambda ((lambda ((var 0) (var 1))) (lambda (lambda ((var 2) (var 1)))))))
+(check-equal? (lex '(lambda (a) (lambda (b) (lambda (c) (lambda (a) (lambda (b) (lambda (d) (lambda (a) (lambda (e) (((((a b) c) d) e) a))))))))) '())
+              '(lambda (lambda (lambda (lambda (lambda (lambda (lambda (lambda ((((((var 1) (var 3)) (var 5)) (var 2)) (var 0)) (var 1)))))))))))
+(check-equal? (lex '(lambda (a) (lambda (b) (lambda (c) (lambda (w) (lambda (x) (lambda (y) ((lambda (a) (lambda (b) (lambda (c) (((((a b) c) w) x) y)))) (lambda (w) (lambda (x) (lambda (y) (((((a b) c) w) x) y))))))))))) '())
+              '(lambda (lambda (lambda (lambda (lambda (lambda ((lambda (lambda (lambda ((((((var 2) (var 1)) (var 0)) (var 5)) (var 4)) (var 3))))) (lambda (lambda (lambda ((((((var 8) (var 7)) (var 6)) (var 2)) (var 1)) (var 0)))))))))))))
