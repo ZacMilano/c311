80e96f067dbc0a0d04d3cf9897b6e3191370f8b6
zacmonroe <zmonroe0820@gmail.com>
2020-03-02 11:59:24 -0500

    Finished a7 lex

--- a7.rkt
+++ a7.rkt
@@ -50,12 +50,16 @@
 (define (lex exp env)
   (match exp
     [`,y #:when (symbol? y) (list 'var (search-env env y))]
+    [`(zero? ,nexp) `(zero ,(lex nexp env))]
+    [`(* ,nexp1 ,nexp2) `(mult ,(lex nexp1 env) ,(lex nexp2 env))]
+    [`(letcc ,kvar ,body) `(letcc ,(lex body (cons kvar env)))]
+    [`(throw ,kexp ,vexp) `(throw ,(lex kexp env) ,(lex vexp env))]
     [`(lambda (,x) ,body)
      (list 'lambda (lex body (cons x env)))]
     [`(,rator ,rand)
      `(app ,(lex rator env) ,(lex rand env))
      #;(list (lex rator env)
-           (lex rand env))]))
+             (lex rand env))]))
 
 (define (search-env env var)
   (cond
@@ -71,15 +75,57 @@
 (check-equal? (search-env '(a b c a) 'a) 0)
 (check-equal? (search-env '(a a a a) 'a) 0)
 
-(check-equal? (lex '(lambda (o) (lambda (r) (lambda (s) (lambda (p) (lambda (g) o))))) '())
-              '(lambda (lambda (lambda (lambda (lambda (var 4)))))))
-(check-equal? (lex '(lambda (x) x) '()) '(lambda (var 0)))
-(check-equal? (lex '(lambda (y) (lambda (x) y)) '()) '(lambda (lambda (var 1))))
-(check-equal? (lex '(lambda (y) (lambda (x) (x y))) '()) '(lambda (lambda ((var 0) (var 1)))))
-(check-equal? (lex '(lambda (x) (lambda (x) (x x))) '()) '(lambda (lambda ((var 0) (var 0)))))
-(check-equal? (lex '(lambda (y) ((lambda (x) (x y)) (lambda (c) (lambda (d) (y c))))) '()) 
-              '(lambda ((lambda ((var 0) (var 1))) (lambda (lambda ((var 2) (var 1)))))))
-(check-equal? (lex '(lambda (a) (lambda (b) (lambda (c) (lambda (a) (lambda (b) (lambda (d) (lambda (a) (lambda (e) (((((a b) c) d) e) a))))))))) '())
-              '(lambda (lambda (lambda (lambda (lambda (lambda (lambda (lambda ((((((var 1) (var 3)) (var 5)) (var 2)) (var 0)) (var 1)))))))))))
-(check-equal? (lex '(lambda (a) (lambda (b) (lambda (c) (lambda (w) (lambda (x) (lambda (y) ((lambda (a) (lambda (b) (lambda (c) (((((a b) c) w) x) y)))) (lambda (w) (lambda (x) (lambda (y) (((((a b) c) w) x) y))))))))))) '())
-              '(lambda (lambda (lambda (lambda (lambda (lambda ((lambda (lambda (lambda ((((((var 2) (var 1)) (var 0)) (var 5)) (var 4)) (var 3))))) (lambda (lambda (lambda ((((((var 8) (var 7)) (var 6)) (var 2)) (var 1)) (var 0)))))))))))))
+(test-runner
+ > (lex '(lambda (x) (x (lambda (y) (x y)))) '())
+ (lambda (app
+          (var 0)
+          (lambda (app (var 1) (var 0)))))
+ > (lex '(lambda (x) (x (lambda (y) (x (* y x))))) '())
+ (lambda (app
+          (var 0)
+          (lambda (app
+                   (var 1)
+                   (mult (var 0) (var 1))))))
+ > (lex '(lambda (x) (x (lambda (y) (zero? (x (* y x)))))) '())
+ (lambda (app (var 0) (lambda (zero (app (var 1) (mult (var 0) (var 1)))))))
+ > (lex '(lambda (x) (x (lambda (y) (zero? (x (* y (lambda (z) (lambda (x) (x (* z y)))))))))) '())
+ (lambda (app
+          (var 0)
+          (lambda (zero
+                   (app (var 1) (mult (var 0) (lambda (lambda (app (var 0) (mult (var 1)
+                                                                                 (var 2)))))))))))
+> (lex '(letcc k (lambda (x) (x (lambda (y) (zero? (x (* y (lambda (z) (lambda (x) (throw k (x (* z y)))))))))))) '())
+(letcc
+  (lambda (app (var 0) (lambda (zero (app (var 1) (mult (var 0) (lambda (lambda (throw (var 4) (app (var 0)
+                                 (mult (var 1) (var 2)))))))))))))
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
