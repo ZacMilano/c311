4675eae79277c00bdd8867ef7746136e3b3b2b02
zacmonroe <zmonroe0820@gmail.com>
2020-03-09 12:23:14 -0400

    Added ack tests; made ack k RI

--- a8.rkt
+++ a8.rkt
@@ -1,4 +1,5 @@
 #lang racket
+(require rackunit)
 
 ; From a6
 (define empty-k
@@ -9,13 +10,33 @@
 	    (error 'empty-k "You can only invoke the empty continuation once")
 	    (begin (set! once-only #t) v))))))
 
-(define ack
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ACK ACK ACK ACK ACK ACK ACK ACK ACK ACK ACK ACK ACK ACK ACK ACK ACK ACK ACK ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define ack-orig
   (lambda (m n k)
     (cond
       [(zero? m) (k (add1 n))]
+      [(zero? n) (ack-orig (sub1 m) 1 k)]
+      [else (ack-orig m (sub1 n) (lambda (v) (ack-orig (sub1 m) v k)))])))
+
+(define ack
+  (lambda (m n k)
+    (cond
+      [(zero? m) (apply-ack-k k (add1 n))]
       [(zero? n) (ack (sub1 m) 1 k)]
-      [else (ack m (sub1 n) (lambda (v) (ack (sub1 m) v k)))])))
- 
+      [else (ack m (sub1 n) (make-ack-k m k))])))
+
+(define make-ack-k
+  (lambda (m k)
+    (lambda (v) (ack (sub1 m) v k))))
+
+ (define apply-ack-k
+  (lambda (k v)
+    (match k
+      [_ (k v)])))
+
 (define ack-reg-driver
   (lambda (m n)
     (error 'ack-reg-driver "leave me alone meanie im not ready yet")
@@ -24,6 +45,26 @@
            [set! ack-n n]
            (ack))))
 
+(define ack-tests-data
+  (list (cons 0 0)
+        (cons 1 1)
+        (cons 0 1)
+        (cons 1 0)
+        (cons 2 0)
+        (cons 3 0)
+        (cons 4 0)
+        (cons 3 1)
+        (cons 3 2)
+        (cons 3 3)
+        (cons 3 4)
+        (cons 3 5)
+        (cons 3 6)))
+
+(for-each
+ (lambda (test-case)
+   (check-equal? (ack (car test-case) (cdr test-case) (empty-k))
+                 (ack-orig (car test-case) (cdr test-case) (empty-k))))
+ ack-tests-data)
 
 
 
